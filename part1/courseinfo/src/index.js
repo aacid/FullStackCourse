7fa1a16dfc6a5c9dc02c7a5cdab7d3a3086a6c91
Oliver <olivercern@gmail.com>
2019-08-30 20:52:02 +0200

    part1/courseinfo/step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,26 +3,26 @@ import ReactDOM from "react-dom";
 
 const App = () => {
     const course = "Half Stack application development";
-    const part1 = {
-        name: "Fundamentals of React",
-        exercises: 10
-    };
-    const part2 = {
-        name: "Using props to pass data",
-        exercises: 7
-    };
-    const part3 = {
-        name: "State of a component",
-        exercises: 14
-    };
+    const parts = [
+        {
+            name: "Fundamentals of React",
+            exercises: 10
+        },
+        {
+            name: "Using props to pass data",
+            exercises: 7
+        },
+        {
+            name: "State of a component",
+            exercises: 14
+        }
+    ];
 
     return (
         <div>
             <Header course={course} />
-            <Content parts={[part1, part2, part3]} />
-            <Total
-                number={part1.exercises + part2.exercises + part3.exercises}
-            />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     );
 };
@@ -53,7 +53,12 @@ const Part = props => {
 const Total = props => {
     return (
         <>
-            <p>Number of exercises {props.number}</p>
+            <p>
+                Number of exercises{" "}
+                {props.parts
+                    .map(part => part.exercises)
+                    .reduce((sum, val) => sum + val)}
+            </p>
         </>
     );
 };
