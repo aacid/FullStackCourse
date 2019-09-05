b6bf476cd99c4f4c75f3942bd5f4cd73ad0dca87
Oliver <olivercern@gmail.com>
2019-09-05 21:17:31 +0200

    part2/phonebook/step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", phone: "00000" }
+    ]);
     const [newName, setNewName] = useState("");
+    const [newPhone, setNewPhone] = useState("");
 
     const handleSubmit = event => {
         event.preventDefault();
         if (persons.some(p => p.name === newName)) {
             alert(`${newName} is already added.`);
         } else {
-            const newPerson = { name: newName };
+            const newPerson = { name: newName, phone: newPhone };
             const newList = persons.concat(newPerson);
             setPersons(newList);
             setNewName("");
+            setNewPhone("");
         }
+    };
+
+    const handleNewName = event => {
+        setNewName(event.target.value);
+    };
+    const handleNewPhone = event => {
+        setNewPhone(event.target.value);
     };
 
     return (
@@ -21,11 +32,10 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    name:{" "}
-                    <input
-                        value={newName}
-                        onChange={event => setNewName(event.target.value)}
-                    />
+                    name: <input value={newName} onChange={handleNewName} />
+                </div>
+                <div>
+                    phone: <input value={newPhone} onChange={handleNewPhone} />
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -33,14 +43,18 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             {persons.map(person => (
-                <Person name={person.name} key={person.name} />
+                <Person person={person} key={person.name} />
             ))}
         </div>
     );
 };
 
-const Person = ({ name }) => {
-    return <p>{name}</p>;
+const Person = ({ person }) => {
+    return (
+        <p>
+            {person.name} {person.phone}
+        </p>
+    );
 };
 
 export default App;
