5673eb2b28c375e5cdd1ae8f48ab8620f2b67e3c
Oliver <olivercern@gmail.com>
2019-09-05 20:35:09 +0200

    part2/phonebook/step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-}
+    const handleSubmit = event => {
+        event.preventDefault();
+        const newPerson = { name: newName };
+        const newList = persons.concat(newPerson);
+        setPersons(newList);
+        setNewName("");
+    };
 
-export default App
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name:{" "}
+                    <input
+                        value={newName}
+                        onChange={event => setNewName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(person => (
+                <Person name={person.name} key={person.name} />
+            ))}
+        </div>
+    );
+};
+
+const Person = ({ name }) => {
+    return <p>{name}</p>;
+};
+
+export default App;
