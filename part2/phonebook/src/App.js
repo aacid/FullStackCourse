33782d95d3cbbc492184b440d37d22d6f5606e3f
Oliver <olivercern@gmail.com>
2019-09-16 21:06:48 +0200

    part2/phonebook/step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Persons from "./components/Persons";
 import NewPerson from "./components/NewPerson";
 import Search from "./components/Search";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456" },
-        { name: "Ada Lovelace", number: "39-44-5323523" },
-        { name: "Dan Abramov", number: "12-43-234345" },
-        { name: "Mary Poppendieck", number: "39-23-6423122" }
-    ]);
+    const [persons, setPersons] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons").then(response => {
+            setPersons(response.data);
+        });
+    }, []);
 
     const [search, setSearch] = useState("");
 
