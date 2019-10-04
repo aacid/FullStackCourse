77d5fbfffc95d185561ba9459512ed23c2d23dc6
Oliver Cernansky <olivercern@gmail.com>
2019-10-04 16:45:12 +0200

    part4/blogexpansion/step9

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -21,6 +21,7 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
