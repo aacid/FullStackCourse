fb2ee6deb099e308421678b0d1797c50235ed37c
Oliver <olivercern@gmail.com>
2019-10-05 16:32:59 +0200

    part5/frontend/step1

--- part4/bloglist-frontend/src/App.js
+++ part4/bloglist-frontend/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import blogsService from "./services/blogs";
+import Blog from "./components/Blog";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [user, setUser] = useState({});
 
+    useEffect(() => {
+        blogsService.getAll().then(blogs => setBlogs(blogs));
+    }, []);
+
+    if (user === null) {
+        return <Login setCredentials={setUser} />;
+    }
+    return (
+        <div>
+            <h3>Blogs</h3>
+            <p>{user.name} logged in.</p>
+            <BlogList blogs={blogs} />
+        </div>
+    );
+};
+
+const BlogList = ({ blogs }) => {
+    return (
+        <div>
+            {blogs.map(blog => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    );
+};
 export default App;
--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,14 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React from "react";
+const Blog = ({ blog }) => {
+    return (
+        <div>
+            <h3>{blog.title}</h3>
+            <p>
+                <a href={blog.url}>link</a>
+            </p>
+            <p>Autor: {blog.user.name}</p>
+        </div>
+    );
+};
 
-export default Blog
+export default Blog;
