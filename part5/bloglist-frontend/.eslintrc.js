7ffd44680d99e6ae56f530151560a3639f486124
Oliver <olivercern@gmail.com>
2019-10-06 22:09:15 +0200

    part5/frontend/step12

---
+++ part5/bloglist-frontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        "jest/globals": true
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    plugins: ["react", "jest"],
+    rules: {
+        "no-console": 0,
+        "react/prop-types": 0
+    }
+};
